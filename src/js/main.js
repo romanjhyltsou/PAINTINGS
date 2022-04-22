e215294a8a4585d8a6431df89fc22687181b8756
jhyltsou <romanjhyltsou@gmail.com>
2022-04-22 13:20:47 +0300

    added_showMoreStyles

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import sliders from "./modules/sliders";
 import forms from "./modules/forms";
 import mask from "./modules/mask";
 import checkTextInputs from "./modules/checkTextInputs";
+import showMoreStyles from "./modules/showMoreStyles";
 
 window.addEventListener('DOMContentLoaded', ()=> {
     'Use strict';
@@ -14,5 +15,8 @@ window.addEventListener('DOMContentLoaded', ()=> {
     mask('[name="phone"]');
     checkTextInputs('[name="name"]');
     checkTextInputs('[name="message"]');  
+    showMoreStyles('.button-styles','.styles-2');
+
+
 });
   
