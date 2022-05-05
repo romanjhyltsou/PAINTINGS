25721225d33cd1c97d8c3aec2ed87a37a52740fe
jhyltsou <romanjhyltsou@gmail.com>
2022-05-05 22:46:11 +0300

    added_db_and_folde_services

--- src/js/modules/showMoreStyles.js
+++ src/js/modules/showMoreStyles.js
@@ -1,8 +1,9 @@
-const showMoreStyles = (trigger, styles)=> {
-    const cards = document.querySelectorAll(styles),
-          btn = document.querySelector(trigger);
+import { getResource } from "../services/requests";
 
-    cards.forEach(card => {
+const showMoreStyles = (trigger, wrapper)=> {
+    const btn = document.querySelector(trigger);
+
+/*     cards.forEach(card => {
         card.classList.add('animated', 'fadeInUp');
     });
 
@@ -12,8 +13,43 @@ const showMoreStyles = (trigger, styles)=> {
             card.classList.add('col-sm-3','col-sm-offset-0','col-xs-10','col-xs-offset-1');
         });
 
-        /* btn.style.display = 'none'; */
+        // btn.style.display = 'none';
         btn.remove();
+    }); */
+
+    btn.addEventListener('click', function(){
+        getResource('http://localhost:3000/styles') /* или assets/db.json */
+            .then(res => createCards(res)) /* и res.styles */
+            .catch(error => console.log(error));
+
+        this.remove();
     });
+
+    function createCards(response){
+        response.forEach( ({src, title, link}) => {
+            let card = document.createElement('div');
+
+            card.classList.add('animated', 'fadeInUp','col-sm-3','col-sm-offset-0','col-xs-10','col-xs-offset-1');
+
+            card.innerHTML = `
+                    <div class='styles-block'>
+                        <img src=${src} alt>
+                        <h4>${title}</h4>
+                        <a href=${link}>Подробнее</a>
+                    </div>
+            `;
+
+            document.querySelector(wrapper).appendChild(card);
+        });
+    }
+
+
+/*     <div class="hidden-lg hidden-md hidden-sm hidden-xs styles-2">
+    <div class=styles-block>
+        <img src=assets/img/styles-7.png alt>
+        <h4>Фотомозаика</h4>
+        <a href="#">Подробнее</a>
+    </div>
+</div> */
 };
 export default showMoreStyles;
